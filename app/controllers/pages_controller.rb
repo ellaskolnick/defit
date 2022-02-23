2625f67b4ed26f41e61244b9bd9263189e1655d2
Andrea Terminel <andrea.terminel@gmail.com>
2022-02-23 11:54:31 +0000

    checkingout of tuesday

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
+ #TBC -  before_action : find_product, only: [ :parameterName ]
+
   def home
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :products
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :products do
+    # /products/...
+    resources
+    resources :reviews, only: [:new, :create]
+  end
+
+    # get :
+
+
+
+  member do
+    get :user
+  end
 end
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
