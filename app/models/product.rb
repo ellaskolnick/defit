a64dcdb584a1a71c3678e29922aad5dbd48e787b
user <user@users-MBP.lewagon>
2022-02-24 15:47:36 +0000

    cloudinary set up update

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_one_attached :photo
   CATEGORIES = %w[Equipment Clothing].freeze
   validates :name, presence: true
   validates :price, presence: true
